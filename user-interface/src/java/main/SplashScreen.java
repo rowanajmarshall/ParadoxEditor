ba911454880f9e48cef5ab158daa86585d218ea6
Rowan Marshall <rowanajmarshall@gmail.com>
2017-05-30 13:41:04 +0100

    Added base Splash Screen file

---
+++ user-interface/src/java/main/SplashScreen.java
@@ -0,0 +1,16 @@
+package java.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SplashScreen extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
